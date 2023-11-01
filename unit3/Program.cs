4f10f25e89503d66318312f908b2647e0b5c311e
irmakruyakdogan <irmakakdogan2@gmail.com>
2023-11-02 00:18:59 +0300

    C# Examples

---
+++ unit3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Unit3
+{
+    static void Main(string[] args) 
+    {
+        int sayi1;
+
+        Console.WriteLine("Bİr sayı girin: ");
+        sayi1 = int.Parse(Console.ReadLine());
+
+        if (sayi1 %2 == 0)
+        {
+            Console.WriteLine($"{sayi1} çifttir.");
+        } else
+        {
+            Console.WriteLine($"{sayi1} tektir");
+        }
+        Console.ReadKey();
+
+
+    }
+}
